f5e0d003aab37f633ef5299a96a3ab186997adf3
barrack3030-wq <barrack3030@gmail.com>
2026-09-11 15:09:47 +0800

    Add editable logo configuration

---
+++ src/data/siteConfig.ts
@@ -0,0 +1,11 @@
+export interface SiteConfig {
+  brandName: string;
+  logoPath: string;
+  logoWidth: number;
+}
+
+export const siteConfig: SiteConfig = {
+  brandName: 'Nakama Digital',
+  logoPath: 'uploads/logo/logo.svg',
+  logoWidth: 150,
+};
